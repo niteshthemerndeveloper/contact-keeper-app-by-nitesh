c8d4097a1b33c9b60c6511901992bdcd416b3b45
Front End Developer <frontendshaala@gmail.com>
2022-01-01 15:36:05 +0530

    Verfiy Login User && Send Token

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,10 @@
+const bcryptjs = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+const config = require('config');
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const router = express.Router();
+const User = require('../models/User.js');
 
 // @route     GET api/auth
 // @desc      Get the Logged in User
@@ -11,8 +16,58 @@ router.get('/', (req, res) => {
 // @route     POST api/auth
 // @desc      Verify & Login a User
 // @access    Public
-router.post('/', (req, res) => {
-  res.send('Login a user from auth.js');
-});
+router.post(
+  '/',
+  [
+    check('email', 'Please enter a valid email').isEmail(),
+    check('password', 'Please enter a valid password').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    // Check if errors not empty
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    let { email, password } = req.body;
+
+    try {
+      // Check users details
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res.status(401).json({ msg: 'Invalid Credentials! U' });
+      }
+
+      // if exist then Check users password
+      const isMatch = await bcryptjs.compare(password, user.password);
+
+      if (!isMatch) {
+        return res.status(401).json({ msg: 'Invalid Credentials! P' });
+      }
+
+      // user verified -> send jwt token
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) throw err;
+          return res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Failed to Respond!');
+    }
+  }
+);
 
 module.exports = router;
